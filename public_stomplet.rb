2455903c56933a305b0c859bece6d46879ce01aa
Bob McWhirter <bob@mcwhirter.org>
2011-08-17 22:36:38 -0400

    Include the sender.

--- public_stomplet.rb
+++ public_stomplet.rb
@@ -15,16 +15,13 @@ class PublicStomplet < TorqueBox::Stomp::JmsStomplet
 
   def on_message(stomp_message, session)
     username = session[:username]
-    puts "username is #{username}"
-    puts "headers are #{stomp_message.headers.class}"
-    #stomp_message.headers['sender'] = username
+    stomp_message.headers['sender'] = username
     stomp_message.headers['recipient'] = 'public'
     send_to( stomp_message, '/topics/chat', :topic )
   end
 
   def on_subscribe(subscriber)
     subscribe_to( subscriber, '/topics/chat', :topic, "recipient='public'" )
-    #subscribe_to( subscriber, '/topics/chat', :topic )
 
     username = subscriber.session[:username]
     ( @roster << username ) unless @roster.include?( username )
