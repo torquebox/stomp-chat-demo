ad18be3aa5b524cf4c13ea4e7a932d70ac919ec7
Bob McWhirter <bob@mcwhirter.org>
2011-08-19 16:01:46 -0400

    Missing this file?

---
+++ chat_demo.rb
@@ -0,0 +1,29 @@
+
+require 'sinatra/base'
+
+class ChatDemo < Sinatra::Base
+
+  helpers TorqueBox::Injectors
+
+  get '/' do
+    haml :login
+  end
+  
+  post '/' do
+    username = params[:username]
+    redirect to( '/' ) and return if username.nil?
+    username.strip!
+    redirect to( '/' ) and return if ( username == '' )
+    redirect to( '/' ) and return if ( ! ( username =~ /^[a-zA-Z0-9_]+$/ ) )
+
+    session[:username] = username
+    haml :chat 
+  end
+
+  get '/profile/:username' do
+    username = params[:username]
+    message = "#{username}, someone from #{env['REMOTE_ADDR']} checked out your profile"
+    inject( '/topics/chat' ).publish( message, :properties=>{ :recipient=>username, :sender=>'system' } )
+    haml :profile
+  end
+end
