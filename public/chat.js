4129f7b426a36253cae58993e6b500ee271449f6
Bob McWhirter <bob@mcwhirter.org>
2011-08-23 10:19:56 -0400

    Less tabs.

--- public/chat.js
+++ public/chat.js
@@ -1,39 +1,38 @@
 
 if (!window.TorqueBox) {
-	window.TorqueBox = {};
-	window.TorqueBox.Events = {};
+  window.TorqueBox = {};
+  window.TorqueBox.Events = {};
 }
 
 $( function() {
 	
-	var chatView = new TorqueBox.ChatView();
-	chatView.initialize();
+  var chatView = new TorqueBox.ChatView();
+  chatView.initialize();
 	
-	var client = Stomp.client(stomp_url);
-	client.connect( null, null, function() {
+  var client = Stomp.client(stomp_url);
+  client.connect( null, null, function() {
 		
-		$(TorqueBox.Events).bind('TorqueBox.Chat.Close', function() { client.disconnect });
-		$(TorqueBox.Events).bind('TorqueBox.Chat.NewMessage', onNewMessage);
+    $(TorqueBox.Events).bind('TorqueBox.Chat.Close', function() { client.disconnect });
+    $(TorqueBox.Events).bind('TorqueBox.Chat.NewMessage', onNewMessage);
 
-		client.subscribe( '/private', function(message) {
-			$(TorqueBox.Events).trigger('TorqueBox.Chat.NewPrivateMessage', [message]);
-    	} );
+    client.subscribe( '/private', function(message) {
+      $(TorqueBox.Events).trigger('TorqueBox.Chat.NewPrivateMessage', [message]);
+    } );
 
-		client.subscribe( '/public', function(message) {
-			$(TorqueBox.Events).trigger('TorqueBox.Chat.NewPublicMessage', [message]);
-		});
-		
-		$(TorqueBox.Events).trigger('TorqueBox.Chat.Connect');
-		
-	});
-	
-	var onNewMessage = function(event, message, recipient) {
-		if ( recipient == 'public' ) {
-			client.send( '/public', {}, $('#input').val() );
-		} else {
-			client.send( '/private', { recipient: recipient }, $('#input').val() );
-		}
-	};
+    client.subscribe( '/public', function(message) {
+      $(TorqueBox.Events).trigger('TorqueBox.Chat.NewPublicMessage', [message]);
+    });
+
+    $(TorqueBox.Events).trigger('TorqueBox.Chat.Connect');
+  });
+
+  var onNewMessage = function(event, message, recipient) {
+    if ( recipient == 'public' ) {
+      client.send( '/public', {}, $('#input').val() );
+    } else {
+      client.send( '/private', { recipient: recipient }, $('#input').val() );
+    }
+  };
 
 } );
 
